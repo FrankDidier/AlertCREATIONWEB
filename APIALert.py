91f1dd6689cf23a9a65913300286206e1cc41263
ISINGIZWE DIDIER FRANK <ididierfrank@yahoo.fr>
2018-01-20 01:08:03 +0800

    Update APIALert.py

--- APIALert.py
+++ APIALert.py
@@ -52,7 +52,7 @@ def makeWebhookResult(req):
         server.starttls()
         server.login("testapiblcu@gmail.com", "Testapi2017")
 
-        msg = "A user need further help at BLCU online Bot -> his/her \nE-mail & his/her Country" +str(Addr)+"\nCountry"+str(Countr) 
+        msg = "A user need further help at BLCU online Bot -> \nE-mail: " +str(Addr)+"\nCountry: "+str(Countr) 
         #+str(ContentM) 
         server.sendmail("testapiblcu@gmail.com", "testapiblcu2017@gmail.com", msg)
         server.quit()
